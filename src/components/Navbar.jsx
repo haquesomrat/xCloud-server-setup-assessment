a0f5bcfa39a54140c31afc055732d614959647a8
haquesomrat <ihsomrat.official@gmail.com>
2024-12-20 14:50:49 +0600

    add a navbar with minimal design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,20 @@
 import xCloudLogo from "/xCloudLogo.png";
+import bellIcon from "/icons/notification-bell.png";
+import profileAvatar from "/profile-avatar.png";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center bg-[#171A30] py-[25px] px-[50px]">
+    <div className="flex justify-between items-center bg-[#171A30] px-4 py-6  md:py-[25px] md:px-[50px]">
       <div>
         <img className="" src={xCloudLogo} alt="Logo" />
       </div>
-      <div className="text-white flex">
-        <h2>Bell Icon</h2>
-        <div>Profile dropdown</div>
+      <div className="text-white inline-flex items-center gap-4">
+        <div>
+          <img className="" src={bellIcon} alt="notification icon" />
+        </div>
+        <div>
+          <img src={profileAvatar} alt="avatar" />
+        </div>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
-import Root from "./layout/root";
+import Root from "./layout/Root";
 import Blueprints from "./pages/Blueprints";
 
 const router = createBrowserRouter([
