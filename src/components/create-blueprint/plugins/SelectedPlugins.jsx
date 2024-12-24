e611650b486e174f91c2d8b23c342a949cc8f3a1
haquesomrat <ihsomrat.official@gmail.com>
2024-12-24 16:33:37 +0600

    plugin select deselect icon reusable style added to index.css

--- src/components/create-blueprint/plugins/SelectedPlugins.jsx
+++ src/components/create-blueprint/plugins/SelectedPlugins.jsx
@@ -2,17 +2,17 @@ import { allPlugins } from "../../../fake-data/allPlugins";
 
 const SelectedPlugins = () => {
   return (
-    <div className="flex flex-wrap items-center gap-x-1 gap-y-4">
+    <div className="flex flex-wrap items-center gap-x-1 gap-y-4 ">
       {allPlugins.map((plugin) => (
         <div
           key={plugin.id}
           style={{ backgroundColor: plugin.bgColor, color: plugin.textColor }}
-          className="border border-transparent rounded-lg h-12 w-12 flex items-center justify-center relative group cursor-pointer hover:-translate-y-[14px] transition-transform duration-300 ease-in-out"
+          className="xc-selected-plugin group"
         >
           <p className="text-2xl font-semibold">{plugin.fallbackIcon}</p>
 
-          {/* selected tick */}
-          <span className="w-4 h-4 flex items-center justify-center border border-xc-foreground bg-[#2DC774] rounded-full absolute -top-[3px] -right-[3px] group-hover:hidden">
+          {/* start::Selected Icon */}
+          <span className="xc-plugin-icon xc-plugin-selected-icon group-hover:hidden">
             <svg
               width="10"
               height="8"
@@ -29,9 +29,10 @@ const SelectedPlugins = () => {
               />
             </svg>
           </span>
+          {/* end::Selected Icon */}
 
-          {/* removal icon */}
-          <span className="w-4 h-4 hidden group-hover:flex items-center justify-center border border-xc-foreground bg-white rounded-full absolute -top-[3px] -right-[3px] ">
+          {/* start::Deselect Icon */}
+          <span className="xc-plugin-icon xc-plugin-deselected-icon group-hover:flex">
             <svg
               width="12"
               height="12"
@@ -62,12 +63,14 @@ const SelectedPlugins = () => {
               </defs>
             </svg>
           </span>
+          {/* end:Deselect Icon */}
 
-          {/* tooltip */}
+          {/* start:Tooltip */}
           <div className="absolute -top-9 text-white bg-[#232A4E] py-1.5 px-3 rounded-lg hidden group-hover:flex whitespace-nowrap">
             <p className="text-xs">Plugin Name</p>
             <div className="absolute left-1/2 -translate-x-1/2 -bottom-2.5 rotate-[270deg] w-0 h-0 border-t-[8px] border-t-transparent border-r-[8px] border-r-[#232A4E]  border-b-[8px] border-b-transparent"></div>
           </div>
+          {/* end:Tooltip */}
         </div>
       ))}
     </div>
